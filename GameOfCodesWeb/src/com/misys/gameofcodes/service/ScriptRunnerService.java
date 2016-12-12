3ef034acdf00839a490900282fa9bfbd05cb66b1
idamser <resma.edgar@gmail.com>
2016-12-12 14:37:26 +0800

    Script runner service

---
+++ GameOfCodesWeb/src/com/misys/gameofcodes/service/ScriptRunnerService.java
@@ -0,0 +1,31 @@
+package com.misys.gameofcodes.service;
+
+import java.net.URISyntaxException;
+
+import com.misys.gameofcodes.model.House;
+import com.misys.gameofcodes.utility.FetchTicket;
+
+public class ScriptRunnerService {
+
+	private ScriptRunnerService() { }
+	private static ScriptRunnerService scriptRunnerService;
+	public static ScriptRunnerService getTicketService(){
+		if (scriptRunnerService == null){
+			scriptRunnerService = new ScriptRunnerService();
+		}
+		return scriptRunnerService;
+	}
+	
+	public void runScripts() {
+		try {
+			FetchTicket ft = new FetchTicket();
+			ft.runScript();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+}
